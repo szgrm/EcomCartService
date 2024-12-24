39034c5a99ea58bb0eb4e6bef15eb8e423333e73
Sezgi İrem İlgar <ilgarsezgiirem@gmail.com>
2024-12-24 22:27:42 +0300

    initial commit

---
+++ EcomCartService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcomCartService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public string Get()
+    {
+        return "TEST";
+    }
+}
